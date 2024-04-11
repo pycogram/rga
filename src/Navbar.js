71e07adea573ca1c21030c43a2f4dad6829fad7b
pycogram <kaluifesinachi@yahoo.com>
2024-04-11 18:16:13 +0100

    made menu to be responsive

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,44 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 let MyNavBar = ( {RGA_logo} ) => {
+    let [ openMenu, setOpenMenu ] = useState("NBOption unclicked");
+    let [ burgerClass, setBurgerClass ] = useState("burgarBar inactive");
+    let [ isMenuClicked, setMenuClicked ] = useState(false);
+    let updateMenu = () => {
+        if(!isMenuClicked){
+            setOpenMenu("NBOption clicked");
+            setBurgerClass("burgarBar active");
+        } else {
+            setOpenMenu("NBOption unclicked");
+            setBurgerClass("burgarBar inactive");
+        }
+        setMenuClicked(!isMenuClicked);
+    }
+    let NavLink = () => {
+        setOpenMenu("NBOption unclicked");
+        setBurgerClass("burgarBar inactive");
+        setMenuClicked(!isMenuClicked);
+    }
     return(
         <header>
             <div className="NBBox">
                     <img src={RGA_logo} alt="RGA Logo"></img> 
-                    <ul className="NBOption">
-                        <li><Link to="/"><h3>Home</h3></Link></li>
-                        <li><Link to="/Overview"><h3>Overview</h3></Link></li>
-                        <li><Link to="/Tokenomic"><h3>Tokenomics</h3></Link></li>
-                        <li><Link to="/Allocation"><h3>Allocation</h3></Link></li>
-                        <li><Link to="/Roadmap"><h3>Roadmap</h3></Link></li>
-                        <li><Link to="/Findus"><h3>FindUs</h3></Link></li>
+                    <ul className={openMenu}>
+                        <li onClick={() => NavLink()}><Link to="/"><h3>Home</h3></Link></li>
+                        <li onClick={() => NavLink()}><Link to="/Overview"><h3>Overview</h3></Link></li>
+                        <li onClick={() => NavLink()}><Link to="/Tokenomic"><h3>Tokenomics</h3></Link></li>
+                        <li onClick={() => NavLink()}><Link to="/Allocation"><h3>Allocation</h3></Link></li>
+                        <li onClick={() => NavLink()}><Link to="/Roadmap"><h3>Roadmap</h3></Link></li>
+                        <li onClick={() => NavLink()}><Link to="/Findus"><h3>FindUs</h3></Link></li>
                     </ul>
                     <div className="NBBtn">
                         <button>Buy $RGA</button>
                         <button>Chart</button>
                     </div>
-                    <div id="MenuBtn">
-                        <span></span>
-                        <span></span>
-                        <span></span>
+                    <div id="MenuBtn" onClick={() => updateMenu()}>
+                        <span className={burgerClass}></span>
+                        <span className={burgerClass}></span>
+                        <span className={burgerClass}></span>
                     </div>
             </div>
         </header>
